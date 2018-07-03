4c784ce09f5e48a5ae528b9a6d5ac0710f44ef64
Adam Kalt <adam.kalt.1@gmail.com>
2018-07-03 18:19:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,15 +12,12 @@ get "/reversename/:name" do
   end
 
   get "/say/:number/:phrase" do
-    @phrase = params[:name]
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
-    @saywords = params[:num1].to_i * params[:num2].to_i
   end
 
   get "/:operation/:number1/:number2" do
-    @operation = params[:num1].to_i * params[:num2].to_i
   end
 
 end
